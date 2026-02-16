d120d13cabf3026cdc7de63748736c32671478e7
SergeiKrivko <sergik.krivko@gmail.com>
2026-02-16 16:44:40 +0300

    group chapters

--- ReportChecker.Api/ReportChecker.AiAgent/AiService.cs
+++ ReportChecker.Api/ReportChecker.AiAgent/AiService.cs
@@ -2,6 +2,7 @@
 using DiffPlex;
 using DiffPlex.DiffBuilder;
 using DiffPlex.DiffBuilder.Model;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using ReportChecker.Abstractions;
 using ReportChecker.Models;
@@ -12,66 +13,73 @@ public class AiService(
     IAiAgentClient aiAgentClient,
     IIssueRepository issueRepository,
     ICommentRepository commentRepository,
-    ILogger<AiService> logger) : IAiService
+    ILogger<AiService> logger,
+    IConfiguration configuration) : IAiService
 {
     private readonly InlineDiffBuilder _diffBuilder = new InlineDiffBuilder(new Differ());
 
     public async Task FindIssuesAsync(Guid checkId, IEnumerable<Chapter> chapters, List<Chapter> existingChapters,
         List<Issue> existingIssues)
     {
-        foreach (var chapter in chapters)
+        var chapterRequests = chapters.Select(c =>
         {
-            logger.LogDebug("Processing chapter {name}", chapter.Name);
-            var existingIssuesForChapter = existingIssues
-                .Where(e => e.Chapter == chapter.Name && e.Status == IssueStatus.Open)
-                .Select(IssueToAgent)
-                .ToArray();
-            var existingChapter = existingChapters.FirstOrDefault(e => e.Name == chapter.Name);
+            var existingChapter = existingChapters.FirstOrDefault(e => e.Name == c.Name);
+            return new ChapterRequest(
+                c.Name,
+                existingChapter?.Content,
+                c.Content,
+                GetDifference(existingChapter?.Content ?? "", c.Content),
+                existingIssues
+                    .Where(e => e.Chapter == c.Name && e.Status == IssueStatus.Open)
+                    .Select(IssueToAgent)
+                    .ToArray()
+            );
+        }).Where(e => e.NewText != e.OldText).ToList();
 
-            if (existingChapter is not null)
+        foreach (var chapterGroup in
+                 GroupChapters(chapterRequests.Where(e => e.OldText != null && e.Issues.Length > 0)))
+        {
+            if (logger.IsEnabled(LogLevel.Information))
+                logger.LogInformation("Processing issues from {count} chapters", chapterGroup.Length);
+
+            var comments = await aiAgentClient.CheckIssues(chapterGroup.Select(c => new IAiAgentClient.Chapter
             {
-                if (existingChapter.Content == chapter.Content)
-                    continue;
-                var diff = GetDifference(existingChapter.Content, chapter.Content);
-                var comments = await aiAgentClient.CheckIssues(new IAiAgentClient.CheckIssuesRequest
-                {
-                    Text = diff,
-                    Issues = existingIssuesForChapter,
-                });
-                foreach (var comment in comments ?? [])
-                {
+                Name = c.Name,
+                Text = c.Difference,
+                Issues = c.Issues,
+            }).ToArray());
+            foreach (var comment in comments ?? [])
+            {
+                if (logger.IsEnabled(LogLevel.Debug))
                     logger.LogDebug("Changing issue {id} status to {status}", comment.IssueId, comment.Status);
-                    await commentRepository.CreateCommentAsync(comment.IssueId, Guid.Empty, comment.Content,
-                        comment.Status is null ? null : Enum.Parse<IssueStatus>(comment.Status));
-                }
+                await commentRepository.CreateCommentAsync(comment.IssueId, Guid.Empty, comment.Content,
+                    comment.Status is null ? null : Enum.Parse<IssueStatus>(comment.Status));
+            }
+        }
 
-                var issues = await aiAgentClient.FindNewIssues(new IAiAgentClient.FindIssuesRequest
-                {
-                    Text = diff,
-                    ExistingIssues = existingIssuesForChapter,
-                });
-                await ProcessIssuesAsync(checkId, chapter.Name, issues ?? []);
-            }
-            else
+        foreach (var chapterGroup in GroupChapters(chapterRequests))
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+                logger.LogDebug("Processing issues from {count} chapters", chapterGroup.Length);
+
+            var issues = await aiAgentClient.FindIssues(chapterGroup.Select(c => new IAiAgentClient.Chapter
             {
-                var issues = await aiAgentClient.FindInitialIssues(new IAiAgentClient.FindIssuesRequest
-                {
-                    Text = chapter.Content,
-                    ExistingIssues = [],
-                });
-                await ProcessIssuesAsync(checkId, chapter.Name, issues ?? []);
-            }
+                Name = c.Name,
+                Text = c.Difference,
+                Issues = c.Issues,
+            }).ToArray());
+            await ProcessIssuesAsync(checkId, issues ?? []);
         }
     }
 
-    private async Task ProcessIssuesAsync(Guid checkId, string chapterName,
-        IEnumerable<IAiAgentClient.IssueCreate> issues)
+    private async Task ProcessIssuesAsync(Guid checkId, IEnumerable<IAiAgentClient.IssueCreate> issues)
     {
         foreach (var issue in issues)
         {
             var issueId =
-                await issueRepository.CreateIssueAsync(checkId, chapterName, issue.Title, issue.Priority);
-            logger.LogDebug("Adding issue '{title}'", issue.Title);
+                await issueRepository.CreateIssueAsync(checkId, issue.Chapter, issue.Title, issue.Priority);
+            if (logger.IsEnabled(LogLevel.Debug))
+                logger.LogDebug("Adding issue '{title}'", issue.Title);
             await commentRepository.CreateCommentAsync(issueId, Guid.Empty, issue.Comment, IssueStatus.Open);
         }
     }
@@ -109,7 +117,7 @@ public class AiService(
     }
 
 
-    public string GetDifference(string oldText, string newText)
+    private string GetDifference(string oldText, string newText)
     {
         var diff = _diffBuilder.BuildDiffModel(oldText, newText);
 
@@ -134,5 +142,28 @@ public class AiService(
         }
 
         return result.ToString();
+    }
+
+    private int MinChapterSize { get; } = int.Parse(configuration["Reports.MinChapterSize"] ?? "50");
+    private int MaxRequestSize { get; } = int.Parse(configuration["Reports.MaxRequestSize"] ?? "5000");
+
+    private IEnumerable<ChapterRequest[]> GroupChapters(IEnumerable<ChapterRequest> chapters)
+    {
+        var lst = new List<ChapterRequest>();
+        var length = 0;
+        foreach (var chapter in chapters)
+        {
+            if (chapter.Difference.Length < MinChapterSize)
+                continue;
+            if (length + chapter.Difference.Length > MaxRequestSize && lst.Count > 0)
+            {
+                yield return lst.ToArray();
+                lst.Clear();
+                length = 0;
+            }
+
+            lst.Add(chapter);
+            length += chapter.Difference.Length;
+        }
     }
 }
---
+++ ReportChecker.Api/ReportChecker.AiAgent/ChapterRequest.cs
@@ -0,0 +1,10 @@
+﻿using ReportChecker.Models;
+
+namespace AiAgent;
+
+internal record ChapterRequest(
+    string Name,
+    string? OldText,
+    string NewText,
+    string Difference,
+    IAiAgentClient.IssueRead[] Issues);
