77b4dc50a0132b2f585d4759a4eb6a3850590b50
SergeiKrivko <sergik.krivko@gmail.com>
2026-02-27 23:04:41 +0300

    Add logs

--- ReportChecker.Api/SourceProviders/ReportChecker.SourceProviders.GitHub/GithubWebhookProcessor.cs
+++ ReportChecker.Api/SourceProviders/ReportChecker.SourceProviders.GitHub/GithubWebhookProcessor.cs
@@ -1,17 +1,24 @@
 ﻿using System.Text.Json;
+using Microsoft.Extensions.Logging;
 using Octokit.Webhooks;
 using Octokit.Webhooks.Events;
 using ReportChecker.Abstractions;
 
 namespace ReportChecker.SourceProviders.GitHub;
 
-public class GithubWebhookProcessor(ICheckService checkService, IReportRepository reportRepository)
+public class GithubWebhookProcessor(
+    ICheckService checkService,
+    IReportRepository reportRepository,
+    ILogger<GithubWebhookProcessor> logger)
     : WebhookEventProcessor
 {
     protected override async ValueTask ProcessPushWebhookAsync(WebhookHeaders headers, PushEvent pushEvent,
         CancellationToken cancellationToken = new())
     {
         await base.ProcessPushWebhookAsync(headers, pushEvent, cancellationToken);
+        if (logger.IsEnabled(LogLevel.Information))
+            logger.LogInformation("Webhook received: repo '{repo}', commit '{commit}'", pushEvent.Repository?.Name,
+                pushEvent.Ref);
 
         var reports = await reportRepository.GetAllReportsOfSourceAsync("GitHub");
         foreach (var report in reports)
@@ -22,6 +29,9 @@ public class GithubWebhookProcessor(ICheckService checkService, IReportRepositor
             if (source == null || source.RepositoryId != pushEvent.Repository?.Id)
                 continue;
             // Check branch
+
+            if (logger.IsEnabled(LogLevel.Information))
+                logger.LogInformation("Adding check to report '{report}'", report.Id);
 
             await checkService.CreateCheckAsync(Guid.Empty, Guid.Empty, JsonSerializer.Serialize(
                 new GitHubCommitSourceSchema
