baf22494f7bb52adbe9ef9d06d2bcf3dfe2b7490
SergeiKrivko <sergik.krivko@gmail.com>
2026-03-10 15:50:27 +0300

    fix github auth

--- ReportChecker.Api/ReportChecker.Models/UserInfo.cs
+++ ReportChecker.Api/ReportChecker.Models/UserInfo.cs
@@ -11,6 +11,7 @@ public class AccountInfo
     public required string Provider { get; init; }
     public required string Id { get; init; }
     public string? Name { get; init; }
+    public string? Login { get; init; }
     public string? Email { get; init; }
     public string? AvatarUrl { get; init; }
 }
