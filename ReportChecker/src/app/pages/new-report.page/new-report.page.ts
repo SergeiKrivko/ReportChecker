2810c2599358c769777ae7796cbe004ac179fdac
SergeiKrivko <sergik.krivko@gmail.com>
2026-03-10 12:50:10 +0300

    ui fixes

--- ReportChecker/src/app/pages/new-report.page/new-report.page.ts
+++ ReportChecker/src/app/pages/new-report.page/new-report.page.ts
@@ -1,11 +1,12 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {Header} from '../../components/header/header';
 import {TuiAvatar} from '@taiga-ui/kit';
-import {TuiAppearance, TuiScrollbar, TuiTitle} from '@taiga-ui/core';
+import {TuiAppearance, TuiButton, TuiScrollbar, TuiTitle} from '@taiga-ui/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {TuiCard} from '@taiga-ui/layout';
 import {FileSpOptions} from '../../components/source-provider-options/file-sp-options/file-sp-options';
 import {GithubSpOptions} from '../../components/source-provider-options/github-sp-options/github-sp-options';
+import {RouterLink} from '@angular/router';
 
 interface SourceProvider {
   key: string;
@@ -26,6 +27,8 @@ interface SourceProvider {
     TuiAppearance,
     FileSpOptions,
     GithubSpOptions,
+    TuiButton,
+    RouterLink,
   ],
   templateUrl: './new-report.page.html',
   styleUrl: './new-report.page.scss',
@@ -54,5 +57,9 @@ export class NewReportPage {
 
   protected selectProvider(provider: SourceProvider) {
     this.selectedProvider = provider;
+  }
+
+  protected deselectProvider() {
+    this.selectedProvider = undefined;
   }
 }
