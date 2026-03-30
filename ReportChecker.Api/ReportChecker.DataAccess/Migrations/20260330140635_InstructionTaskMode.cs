d7fd0b9a4d67a7fa15574926da63ff1d19880ca3
SergeiKrivko <sergik.krivko@gmail.com>
2026-03-30 19:45:32 +0300

    migration

---
+++ ReportChecker.Api/ReportChecker.DataAccess/Migrations/20260330140635_InstructionTaskMode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportChecker.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InstructionTaskMode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Mode",
+                table: "InstructionTasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mode",
+                table: "InstructionTasks");
+        }
+    }
+}
