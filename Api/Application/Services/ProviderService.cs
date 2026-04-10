4a14892024d2fc1a07a50a5dfcdda19ab1346f62
SergeiKrivko <sergik.krivko@gmail.com>
2026-04-10 23:45:26 +0300

    change project structure

--- Api/Application/Services/ProviderService.cs
+++ Api/Application/Services/ProviderService.cs
@@ -1,8 +1,4 @@
 ﻿using ReportChecker.Abstractions;
-using ReportChecker.FormatProviders.Latex;
-using ReportChecker.FormatProviders.Pdf;
-using ReportChecker.SourceProviders.File;
-using ReportChecker.SourceProviders.GitHub;
 using IFormatProvider = ReportChecker.Abstractions.IFormatProvider;
 
 namespace ReportChecker.Application.Services;
