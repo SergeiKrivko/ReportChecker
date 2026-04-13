4f844bdee7fdc7cea6abec5f7c90e71e7dc63972
SergeiKrivko <sergik.krivko@gmail.com>
2026-04-13 19:43:32 +0300

    fix DI

---
+++ Api/DataAccess/Repositories/LocalCheckSourceRepository.cs
@@ -0,0 +1,30 @@
+﻿using ReportChecker.DataAccess.Entities;
+using ReportChecker.Models.Sources;
+
+namespace ReportChecker.DataAccess.Repositories;
+
+public class LocalCheckSourceRepository(ReportCheckerDbContext dbContext)
+    : BaseCheckSourceRepository<LocalCheckSource, LocalCheckSourceEntity>(dbContext.LocalCheckSources, dbContext)
+{
+    protected override LocalCheckSource FromEntity(LocalCheckSourceEntity entity)
+    {
+        return new LocalCheckSource
+        {
+            FileName = entity.FileName,
+            CreatedAt = entity.CreatedAt,
+            DeletedAt = entity.DeletedAt,
+        };
+    }
+
+    protected override LocalCheckSourceEntity ToEntity(Guid id, Guid? checkId, LocalCheckSource data)
+    {
+        return new LocalCheckSourceEntity
+        {
+            Id = id,
+            CheckId = checkId,
+            FileName = data.FileName,
+            CreatedAt = data.CreatedAt,
+            DeletedAt = data.DeletedAt,
+        };
+    }
+}
