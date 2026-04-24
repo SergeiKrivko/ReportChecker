f1afaddcb02d1b11347a137f26b5d4c001a85e9d
SergeiKrivko <sergik.krivko@gmail.com>
2026-04-25 01:09:46 +0300

    limit in percentages

--- ReportChecker/src/app/components/subscription-limit/subscription-limit.ts
+++ ReportChecker/src/app/components/subscription-limit/subscription-limit.ts
@@ -2,7 +2,7 @@ import {Component, input} from '@angular/core';
 import {TuiChip, TuiInputRange, TuiProgressBar} from '@taiga-ui/kit';
 import {LimitEntity} from '../../entities/current-subscription-entity';
 import {map, Observable} from 'rxjs';
-import {TuiAppearance, TuiAppearanceOptions} from '@taiga-ui/core';
+import {TuiAppearanceOptions} from '@taiga-ui/core';
 import {toObservable} from '@angular/core/rxjs-interop';
 import {AsyncPipe} from '@angular/common';
 
@@ -13,13 +13,13 @@ import {AsyncPipe} from '@angular/common';
     TuiChip,
     AsyncPipe,
     TuiProgressBar,
-    TuiAppearance
   ],
   templateUrl: './subscription-limit.html',
   styleUrl: './subscription-limit.scss',
 })
 export class SubscriptionLimit {
   name = input<string>();
+  percentages = input<boolean>(false);
   limit = input.required<LimitEntity<number>>();
 
   protected readonly appearance$: Observable<TuiAppearanceOptions["appearance"]> = toObservable(this.limit).pipe(
